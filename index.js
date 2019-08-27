f33c290db05c73f18c8e167e7efd2259ab6c8218
Stepan <filthy.and.filthy@gmail.com>
2019-08-28 00:14:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 console.log('aaa fire')
 const myJson = fetch('./db/index.json').then(r=>r.json()).then(d=>console.log(JSON.stringify(d)))
+const myJson2 = fetch('./db/index2.json').then(r=>r.json()).then(d=>console.log(JSON.stringify(d)))
